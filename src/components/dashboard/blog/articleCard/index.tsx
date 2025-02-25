b364ac4e913687273004b3cea48874c0ce87d0af
RafaelFrire <rafaelfesousa@hotmail.com>
2025-02-24 23:46:34 -0300

    feat: click on article navigate to content

--- src/components/dashboard/blog/articleCard/index.tsx
+++ src/components/dashboard/blog/articleCard/index.tsx
@@ -14,31 +14,29 @@ export const ArticleCard: React.FC<articleCardProps> = ({
   date,
   imageUrl,
   categorie,
-  slug
+  slug,
 }) => {
   return (
     <a href={`/dashboard/blog/${categorie}/${slug}`}>
-    <div className="flex flex-col items-center w-full h-[350px] border-t-2 border-r-2 border-l-2 rounded-lg cursor-pointer border-primary100 bg-gray-200 overflow-hidden"
-    >
-      <div className="w-full h-[80%]">
-        <Image
-          src={imageUrl}
-          alt="article"
-          width={500}
-          height={300}
-          className="w-full h-full object-cover"
-        />
-      </div>
-      {/* Container do texto */}
-      <div className="w-full h-[35%] flex flex-col justify-center p-2 text-start px-4 rounded-t-none rounded-b-lg rounded-l-md rounded-r-md border-2 border-primary100">
-        <div className="flex items-center gap-1">
-          <Calendar width={20} height={20} color="#B80000" />
-          <h1 className="text-sm text-gray-600 font-semibold">{date}</h1>
+      <div className="flex flex-col items-center w-full h-[350px] border-t-2 border-r-2 border-l-2 rounded-lg cursor-pointer border-primary100 bg-gray-200 overflow-hidden">
+        <div className="w-full h-[80%]">
+          <Image
+            src={imageUrl}
+            alt="article"
+            width={500}
+            height={300}
+            className="w-full h-full object-cover"
+          />
         </div>
-        <h1 className="text-xl font-bold text-primary100">{title}</h1>
+        {/* Container do texto */}
+        <div className="w-full h-[35%] flex flex-col justify-center p-2 text-start px-4 rounded-t-none rounded-b-lg rounded-l-md rounded-r-md border-2 border-primary100">
+          <div className="flex items-center gap-1">
+            <Calendar width={20} height={20} color="#B80000" />
+            <h1 className="text-sm text-gray-600 font-semibold">{date}</h1>
+          </div>
+          <h1 className="text-xl font-bold text-primary100">{title}</h1>
+        </div>
       </div>
-    </div>
     </a>
-
   );
 };
--- src/components/dashboard/blog/lastArticleCard/index.tsx
+++ src/components/dashboard/blog/lastArticleCard/index.tsx
@@ -9,23 +9,27 @@ export const LastArticleCard: React.FC<LastArticleCardProps> = ({
   article,
 }) => {
   return (
-    <div className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition-shadow cursor-pointer">
-      {/* Imagem do artigo */}
-      <Image
-        src={article.imageUrl}
-        alt={article.title}
-        className="w-24 h-24 object-cover rounded-lg"
-        width={200}
-        height={200}
-      />
+    <a href={`/dashboard/blog/${article.categorie}/${article.slug}`}>
+      <div className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition-shadow cursor-pointer">
+        {/* Imagem do artigo */}
+        <Image
+          src={article.imageUrl}
+          alt={article.title}
+          className="w-24 h-24 object-cover rounded-lg"
+          width={200}
+          height={200}
+        />
 
-      {/* Conteúdo do artigo */}
-      <div className="flex flex-col justify-between">
-        <span className="text-sm text-gray-500">
-          {new Date(article.date).toLocaleDateString("pt-BR")}
-        </span>
-        <h2 className="text-sm font-semibold text-gray-800">{article.title}</h2>
+        {/* Conteúdo do artigo */}
+        <div className="flex flex-col justify-between">
+          <span className="text-sm text-gray-500">
+            {new Date(article.date).toLocaleDateString("pt-BR")}
+          </span>
+          <h2 className="text-sm font-semibold text-gray-800">
+            {article.title}
+          </h2>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };

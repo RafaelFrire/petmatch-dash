588f24c185676ef309c6583769da07ae368bf7df
RafaelFrire <rafaelfesousa@hotmail.com>
2025-04-14 23:58:29 -0300

    fix: cleaning code

--- src/components/pages/home/AboutOrganizationsSection/index.tsx
+++ src/components/pages/home/AboutOrganizationsSection/index.tsx
@@ -1,18 +1,12 @@
 import { FC } from "react";
 import { SectionText } from "./SectionText";
 import { SectionImages } from "./SectionImages";
-import { TitleWithPaw } from "@/components/TitleWithPaw";
 
 export const AboutOrganizationsSection: FC = () => {
   return (
-    <>
-      <div className="text-6xl font-bold text-center mb-8">
-        <TitleWithPaw title="Eventos" />
-      </div>
-      <section className="w-full py-16 flex flex-col md:flex-row gap-8 justify-between">
-        <SectionText />
-        <SectionImages />
-      </section>
-    </>
+    <section className="w-full py-16 flex flex-col md:flex-row gap-8 justify-between">
+      <SectionText />
+      <SectionImages />
+    </section>
   );
 };
